43c02c2e98bc45b3d7be13e90318bc18bdbbbba5
Andrew Pedley <apedley2006@gmail.com>
2022-04-28 08:03:44 -0400

    third commit

--- 4-24-22_codewars_solutions.js
+++ 4-24-22_codewars_solutions.js
@@ -4,3 +4,10 @@ function multipleOfIndex(array) {
     // good luck
     return array.filter((e, i) => e % i === 0)
   }
+
+// CSV representation of array 8 kyu
+
+function toCsvText(array) {
+  // good luck
+ return array.join('\n')
+}
