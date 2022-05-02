e599975e570a9388c15ec87b685cec28899cc8e5
Andrew Pedley <apedley2006@gmail.com>
2022-05-02 07:43:46 -0400

    fifth commit

--- 4-24-22_codewars_solutions.js
+++ 4-24-22_codewars_solutions.js
@@ -18,3 +18,13 @@ function multiTable(number) {
   // good luck
   return [1,2,3,4,5,6,7,8,9,10].map(item => `${item} * ${number} = ${item*number}`).join('\n')
 }
+
+// Type of sum 8 kyu
+const typeOfSum = (a, b) => typeof (a + b)
+
+// Array plus array 8 kyu
+function arrayPlusArray(arr1, arr2) {
+  let arr3 = arr1.map((num, idx) => num + arr2[idx])
+  
+  return arr3.reduce((a, b) => a+b)
+}
